cbaabad6b202063bde452703821ad381c98dc054
Verboven Koen <koenverboven@gmail.com>
2025-02-03 15:11:10 +0100

    add UserRole class

---
+++ Persistent/BLL/Models/UserRole.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace AppCode.BLL.Models
+{
+    public enum Role
+    {
+        Admin,
+        student,
+        parent,
+        teacher,
+        developper
+    }
+
+    internal class UserRole
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public IList<Role>? RolesList { get; set; }
+    }
+}
